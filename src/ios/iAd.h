15420121bcd8b07ae6d969d1c5d0190f12299854
cranberrygame <cranberrygame@yahoo.com>
2014-07-26 02:01:46 +0900

    .

--- src/ios/iAd.h
+++ src/ios/iAd.h
@@ -1,3 +1,11 @@
+// Copyright (c) 2014 cranberrygame
+// Email: cranberrygame@yahoo.com
+// Phonegap plugin: http://www.github.com/cranberrygame
+// Construct2 phonegap plugin: https://www.scirra.com/forum/viewtopic.php?f=153&t=109586
+//                             https://dl.dropboxusercontent.com/u/186681453/index.html
+//                             https://www.scirra.com/users/cranberrygame
+// Facebook: https://www.facebook.com/profile.php?id=100006204729846
+// License: MIT (http://opensource.org/licenses/MIT)
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
 //
